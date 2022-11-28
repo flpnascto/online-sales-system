c4deac579bca87ea9914b35b243f8da10e38b2bf
Felipe Melo Meireles do Nascimento <flpnascto@gmail.com>
2022-11-28 17:52:58 -0300

    test: add cases to cpf validator

--- tests/validateCPF.test.ts
+++ tests/validateCPF.test.ts
@@ -1,31 +1,53 @@
 import validate from '../src/utils/validateCPF';
 
-test('Verifica se um CPF no formato string é valido', () => {
+const validCpfs = [
+	"987.654.321-00",
+	"714.602.380-01",
+	"313.030.210-72",
+	"144.796.170-60"
+];
+
+test.each(validCpfs)('Verifica se o CPF %s no formato string é valido', () => {
   const validCPF = '111.444.777-35';
-  expect(validate(validCPF)).toBe(true)
+  expect(validate(validCPF)).toBe(true);
 })
 
 test('Verifica se um CPF no formato numérico é valido', () => {
   const validCPF: number = 11144477735;
-  expect(validate(validCPF.toString())).toBe(true)
+  expect(validate(validCPF.toString())).toBe(true);
 })
 
-describe('Verifica se um CPF inválido retorna false', () => {
-  test('CPF com menos de 11 caracteres', () => {
-    const cpf = '111.444.777-3'
-    expect(validate(cpf)).toBe(false)
+const invalidCpfs = [
+	"111.111.111-11",
+	"222.222.222-22",
+	"333.333.333-33",
+	"444.444.444-44",
+	"555.555.555-55",
+	"666.666.666-66",
+	"777.777.777-77",
+	"888.888.888-88",
+	"999.999.999-99",
+	"987.654.321-01",
+	"714.602.380-10",
+	"313.030.210-70",
+	"144.796.170-10"
+];
+
+describe('Verifica se retorna false quando', () => {
+  test('o CPF tem menos de 11 caracteres', () => {
+    const cpf = '111.444.777-3';
+    expect(validate(cpf)).toBeFalsy();
   });
-  test('CPF com mais de 14 caracteres', () => {
-    const cpf = '111.444.777-351'
-    expect(validate(cpf)).toBe(false)
+  test('o CPF tem mais de 14 caracteres', () => {
+    const cpf = '111.444.777-351';
+    expect(validate(cpf)).toBeFalsy();
   });
-  test('CPF tem valor vazio', () => {
+  test('o CPF tem valor vazio', () => {
     const cpf = '';
+    expect(validate(cpf)).toBeFalsy();
+  });
+  test.each(invalidCpfs)('o CPF é inválido: %s', () => {
+    const cpf = '111.444.777-34';
     expect(validate(cpf)).toBe(false)
   });
-  test('CPF com dígito verificador inválido', () => {
-    const cpf = '111.444.777-34'
-    expect(validate(cpf)).toBe(false)
-  });
-  
 })
