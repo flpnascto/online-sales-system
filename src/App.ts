295d31e56cd08e5cc9346f33821f872724b4295d
Felipe Melo Meireles do Nascimento <flpnascto@gmail.com>
2022-12-04 12:47:12 -0300

    feat: update App

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,23 @@
 import express, { Request, Response } from 'express';
+import pgp from "pg-promise";
 
+const db = pgp()("postgres://postgres:postgres@localhost:3002");
+
+interface IProduct {
+  id: number;
+  description: string;
+  price: number;
+}
+
+interface IOrderProduct {
+  productId: number;
+  quantity: number;
+}
+interface IOrder {
+  id: number;
+  products: IOrderProduct[];
+  totalPrice:number;
+}
 class App {
   public app: express.Express;
 
@@ -8,14 +26,24 @@ class App {
 
     this.config();
 
-    this.app.get('/', (req: Request, res: Response) => res.json({ ok: true }));
-    this.app.post('/orders', (req: Request, res: Response) => {
-      const products = req.body;
-      const newOrder = {
-        id: 1,
-        products,
-        totalPrice: 140
-      }
+    this.app.get('/', (_req: Request, res: Response) => res.json({ ok: true }));
+    this.app.post('/orders', async (req: Request, res: Response) => {
+      // const orderProducts = req.body as IOrderProduct[];
+      let totalPrice = 0;
+      for (const item of req.body.itens) {
+        const [product] = await db.query<IProduct[]>("SELECT * FROM sales_system.products where id = $1", [item.productId]);
+        if (product) {
+          totalPrice += product.price * item.quantity;
+        } else {
+          return res.status(422).json({
+            message: "Product not found"
+          });
+        }
+      };
+
+      const { id } = await db.one('INSERT INTO sales_system.orders(total_price) VALUES($1) RETURNING id', [totalPrice])
+
+      const newOrder = { id: 1, totalPrice }
       return res.status(201).json(newOrder)
     });
 
