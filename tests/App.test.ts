cd002533af957113dbce9ce3cb3a010e4e9924ef
Felipe Melo Meireles do Nascimento <flpnascto@gmail.com>
2022-12-04 12:57:53 -0300

    test: order with cupom

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -31,3 +31,24 @@ test('Deve criar um pedido com 3 produtos (com descrição, preço e quantidade)
   expect(response.status).toBe(201);
   expect(response.data).toMatchObject(output)
 })
+
+test('Deve criar um pedido com 3 produtos, com cupom de desconto e calcular o valor total', async () => {
+  const input = {
+    cpf: '987.654.321-00',
+    itens: [
+      { productId: 1, quantity: 1 },
+      { productId: 2, quantity: 2 },
+      { productId: 3, quantity: 3 },
+    ],
+    cupom: "CUPOM10",
+  }
+  const output = {
+    id: 1,
+    totalPrice: 126,
+  };
+  const response = await axios.post(ENDPOINT_ORDERS, input);
+  console.log(response);
+  
+  expect(response.status).toBe(201);
+  expect(response.data).toMatchObject(output)
+})
