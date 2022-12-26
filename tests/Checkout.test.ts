f9418cadfcffe9ba054e229382185381f0d548e1
Felipe Melo Meireles do Nascimento <flpnascto@gmail.com>
2022-12-26 13:55:26 -0300

    test: order with different currency

--- tests/Checkout.test.ts
+++ tests/Checkout.test.ts
@@ -4,14 +4,15 @@ import MailerConsole from "../src/MailerConsole";
 import sinon from "sinon";
 import ProductData from "../src/ProductData";
 import CouponData from "../src/CouponData";
+import CurrencyGateway from "../src/CurrencyGatewayRandom";
 
 const productData: ProductData = {
   async getProduct(idProduct: number): Promise<any> {
     const products: { [idProduct: number]: any } = {
-      1: { idProduct: 1, description: "A", price: 10, width: 10, height: 10, length: 10, weight: 0.9 },
-      2: { idProduct: 2, description: "B", price: 20, width: 100, height: 30, length: 10, weight: 3 },
-      3: { idProduct: 3, description: "C", price: 30, width: 200, height: 100, length: 50, weight: 40 },
-
+      1: { idProduct: 1, description: "A", price: 10, width: 10, height: 10, length: 10, weight: 0.9, currency: "BRL" },
+      2: { idProduct: 2, description: "B", price: 20, width: 100, height: 30, length: 10, weight: 3, currency: "BRL" },
+      3: { idProduct: 3, description: "C", price: 30, width: 200, height: 100, length: 50, weight: 40, currency: "BRL" },
+      4: { idProduct: 4, description: "D", price: 100, width: 100, height: 30, length: 10, weight: 3, currency: "USD" },
     }
     return products[idProduct];
   }
@@ -56,5 +57,31 @@ test('Deve enviar um email ao final do checkout', async () => {
   await checkout.execute(input);
   expect(mailerSpy.calledOnce).toBeTruthy();
   expect(mailerSpy.calledWith('test@example.com', 'Checkout Success', 'Total price: 580')).toBeTruthy();
+  mailerSpy.restore();
+})
+
+test("Deve fazer um pedido com 4 produtos com moedas diferentes", async function () {
+  const currencyGatewayStub = sinon.stub(CurrencyGateway.prototype, "getCurrencies").resolves({
+    "BRL": 1,
+    "USD": 2,
+  });
+  const mailerSpy = sinon.spy(MailerConsole.prototype, 'send');
+  const input = {
+    cpf: '987.654.321-00',
+    itens: [
+      { productId: 1, quantity: 1 },
+      { productId: 2, quantity: 2 },
+      { productId: 3, quantity: 3 },
+      { productId: 4, quantity: 1 },
+
+    ],
+    email: 'test@example.com'
+  }
+  const checkout = new Checkout(productData, couponData);
+  const output = await checkout.execute(input);
+  expect(output.totalPrice).toBe(810);
+  expect(mailerSpy.calledOnce).toBeTruthy();
+  expect(mailerSpy.calledWith('test@example.com', 'Checkout Success', 'Total price: 810')).toBeTruthy();
+  currencyGatewayStub.restore();
   mailerSpy.restore();
 })
